f9e63d465197b03cd1e24e94c0898aa5a28b2edd
MatthieuCrouzet <mattcrouzet@hotmail.fr>
2017-03-13 17:10:40 -0400

    all design pattern

--- src/utils/Pattern.java
+++ src/utils/Pattern.java
@@ -4,5 +4,30 @@ package utils;
  * Created by Matthieu CROUZET on 04/03/2017.
  */
 public enum Pattern {
-    OBSERVER, MEDIATOR, MVC
+    OBSERVER,
+    /*
+    MEDIATOR,
+    MVC,
+    ITERATOR,
+    FACTORY_METHOD,
+    ABSTRACT_FACTORY,
+    COMMAND,
+    COMPOSITE,
+    SINGLETON,
+    ADAPTER,
+    DECORATOR,
+    STRATEGY,
+    VISITOR,
+    PROXY,
+    FLYWEIGHT,
+    MEMENTO,
+    PROTOTYPE,
+    CHAIN_OF_RESPONSIBILITY,
+    STATE,
+    TEMPLATE_METHOD,
+    INTERPRETER,
+    BRIDGE,
+    BUILDER,
+    FACADE,
+ */
 }
